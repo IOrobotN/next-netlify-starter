51c475c88a189643e739efdfd0e531aa505fc056
IOrobotN <95096752+IOrobotN@users.noreply.github.com>
2024-05-01 12:58:10 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,15 @@
-<!-- Header -->
 <header>
-  <!-- Image -->
   <figure class="aligncenter">
     <img src="https://madeinnickmy.files.wordpress.com/2024/04/madeinnick.png?w=1024" alt="" style="width:698px;height:auto"/>
   </figure>
 
-  <!-- Posts Carousel -->
   <!-- This will require custom implementation or a JavaScript library to achieve the same functionality -->
 
-  <!-- Contact Me -->
   <h4>If you want to contact me, go to the contact page</h4>
   <div class="buttons">
     <a class="button" href="https://madeinnickmy.wordpress.com/contattami/">Go to contact page</a>
   </div>
 
-  <!-- View All Blogs -->
   <h4>If you want to see all published blogs (tutorials, tips…), go to the all blogs page or search for what you need</h4>
   <div class="buttons">
     <a class="button" href="https://madeinnickmy.wordpress.com/blog/">Go to all blogs page</a>
