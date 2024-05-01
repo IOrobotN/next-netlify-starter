929438c9aeb7208da18cd8c6323d439987f5e822
IOrobotN <95096752+IOrobotN@users.noreply.github.com>
2024-05-01 12:54:04 +0200

    index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,22 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+<!-- Header -->
+<header>
+  <!-- Image -->
+  <figure class="aligncenter">
+    <img src="https://madeinnickmy.files.wordpress.com/2024/04/madeinnick.png?w=1024" alt="" style="width:698px;height:auto"/>
+  </figure>
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  <!-- Posts Carousel -->
+  <!-- This will require custom implementation or a JavaScript library to achieve the same functionality -->
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+  <!-- Contact Me -->
+  <h4>If you want to contact me, go to the contact page</h4>
+  <div class="buttons">
+    <a class="button" href="https://madeinnickmy.wordpress.com/contattami/">Go to contact page</a>
+  </div>
 
-      <Footer />
-    </div>
-  )
-}
+  <!-- View All Blogs -->
+  <h4>If you want to see all published blogs (tutorials, tips…), go to the all blogs page or search for what you need</h4>
+  <div class="buttons">
+    <a class="button" href="https://madeinnickmy.wordpress.com/blog/">Go to all blogs page</a>
+  </div>
+</header>
